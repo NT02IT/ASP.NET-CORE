15da10048084a6868f326dd8657d4d2dc7e8aad5
666Silver <94086077+666Silver@users.noreply.github.com>
2024-11-15 17:49:08 +0700

    Done bài tập tuần

---
+++ Sources/ContactManagerApp/Models/Contact.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactManagerApp.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
---
+++ Sources/TaskManagerApp/Program.cs
@@ -0,0 +1,27 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Task/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+// Set TaskController as the default controller
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Task}/{action=Index}/{id?}");
+
+app.Run();
