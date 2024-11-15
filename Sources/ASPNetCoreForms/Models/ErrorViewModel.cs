da01dd6e1990854a53aa3714859929e94290df5a
Low Tee <lowtee.vn@gmail.com>
2024-11-15 16:52:52 +0700

    Done 5 6 7

---
+++ Sources/ASPNetCoreForms/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ASPNetCoreForms.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
---
+++ Sources/DependencyInjection1/Startup.cs
@@ -0,0 +1,39 @@
+﻿using DependencyInjection1.Services;
+using Microsoft.AspNetCore.Builder;
+using static DependencyInjection1.Services.SomeService;
+
+namespace DependencyInjection1
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+
+            // Đăng ký IProductService và ProductService trong DI container
+            services.AddTransient<ITransientService, SomeService>();
+            services.AddScoped<IScopedService, SomeService>();
+            services.AddSingleton<ISingletonService, SomeService>();
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.Run(async context => {
+                context.Response.StatusCode = StatusCodes.Status404NotFound;
+                await context.Response.WriteAsync("Page not found");
+            });
+        }
+    }
+}
